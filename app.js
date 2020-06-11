920424fd80c3f67bab4382130da5a26846e80494
sushmitc <zz10sushmit@gmail.com>
2020-06-11 18:14:32 +0530

    11th june

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 require('./src/db/mongoose');
+const cors = require('cors');
 const Engineer_Router = require('./src/routes/engineer_routes');
 const Project_Router = require('./src/routes/project_route');
 const ProjectCard_Router = require('./src/routes/project_card_route');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/engineer', Engineer_Router);
