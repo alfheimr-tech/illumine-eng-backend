2bc13de24bb7bf818c001bb018322d501696cce6
sushmitc <zz10sushmit@gmail.com>
2020-06-20 18:08:50 +0530

    20th june

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const ProjectCard_Router = require('./src/routes/project_card_route');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 app.use('/api/engineer', Engineer_Router);
