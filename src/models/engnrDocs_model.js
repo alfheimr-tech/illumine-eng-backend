7fd571bd8d381dca313d917dd39f346281242072
sushmitc <zz10sushmit@gmail.com>
2020-07-11 16:27:36 +0530

    11th july

--- src/models/engnrDocs_model.js
+++ src/models/engnrDocs_model.js
@@ -18,6 +18,11 @@ const documentSchema = new mongoose.Schema({
         trim: true
       },
 
+      docType: {
+        type: String,
+        trim: true
+      },
+
       url: {
         type: String,
         trim: true
